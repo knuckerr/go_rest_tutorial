6b36b592d64a9ac328ed3e9de336e62e8358b397
knuckerr <knucker@localhost.localdomain>
2019-12-29 13:56:15 +0200

    feat:add middleware auth to the endpoints

--- api/middlewares/root.go
+++ api/middlewares/root.go
@@ -1,12 +1,29 @@
 package middlewares
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/knuckerr/go_rest/api/auth"
 	"net/http"
+	"strings"
 )
 
-func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		next.ServeHTTP(w, r)
+func AuthenticationRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		reqToken := c.Request.Header.Get("Authorization")
+		splitToken := strings.Split(reqToken, "Bearer")
+		if len(splitToken) == 1 {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "user needs to be signed in to access this service"})
+			c.Abort()
+			return
+		}
+		reqToken = strings.TrimSpace(splitToken[1])
+		var claims = &auth.Claims{}
+		err := auth.Vaildtoken(reqToken, claims)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "user needs to be signed in to access this service"})
+			c.Abort()
+			return
+		}
+		c.Next()
 	}
 }
