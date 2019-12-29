b712f2ac30727693980cad52b67aea7b6d36032a
knuckerr <knucker@localhost.localdomain>
2019-12-29 13:19:51 +0200

    feat:add login and refresh endpoint to create token and refresh it

--- api/auth/token.go
+++ api/auth/token.go
@@ -25,31 +25,40 @@ func Createtoken(u *models.User) (string, error) {
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(viper.GetString("SECRET_KEY"))
+	tokenString, err := token.SignedString([]byte(viper.GetString("SECRET_KEY")))
 	if err != nil {
 		return "", err
 	}
 	return tokenString, nil
 }
 
-func Refreshtoken(token string) (string, error) {
-	claims := &Claims{}
-	tkn, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
-		return viper.GetString("SECRET_KEY"), nil
+func Vaildtoken(tokenstring string, claims *Claims) error {
+	tkn, err := jwt.ParseWithClaims(tokenstring, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(viper.GetString("SECRET_KEY")), nil
 	})
 	if !tkn.Valid {
-		return "", errors.New("Invalid token")
+		return errors.New("Invalid token")
 	}
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func Refreshtoken(tokenstring string) (string, error) {
+	claims := &Claims{}
+	err := Vaildtoken(tokenstring, claims)
 	if err != nil {
 		return "", err
 	}
 	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
 		return "", errors.New("token must be older than 30 sec")
 	}
-	expirationTime := time.Now().Add(5 * time.Minute)
+	expirationTime := time.Now().Add(40 * time.Minute)
 	claims.ExpiresAt = expirationTime.Unix()
 	token_new := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token_new.SignedString(viper.GetString("SECRET_KEY"))
+	tokenString, err := token_new.SignedString([]byte(viper.GetString("SECRET_KEY")))
 	return tokenString, nil
 
 }
--- api/controllers/routers.go
+++ api/controllers/routers.go
@@ -3,6 +3,11 @@ package controllers
 func (server *Server) InitializeRoutes() {
 	v1 := server.Router.Group("/v1")
 	{
+		auth := v1.Group("/auth")
+		{
+			auth.POST("/login", server.Login)
+			auth.POST("/refresh", server.RefreshToken)
+		}
 		users := v1.Group("/users")
 		{
 			users.GET("/", server.GetUsers)
